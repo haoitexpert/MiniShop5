4941c729d79c4e4ee60f686b86500bdafbbe198d
HAO LUONG <luongvihao@outlook.com>
2026-01-21 21:50:05 +0700

    Lab 1: Form input + binding

---
+++ Models/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace MiniShop5.Models
+{
+    public enum Category
+    {
+        Food,
+        Drink,
+        Electronics
+    }
+
+    public class Product
+    {
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+        public Category Category { get; set; } = Category.Food;
+        public bool IsActive { get; set; }
+        public DateTime AvailableAfter { get; set; } = DateTime.Today;
+    }
+}
